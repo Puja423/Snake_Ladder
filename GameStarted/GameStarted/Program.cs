5a6a4cf10a4191a030a3d6bb1a3d764cfe04f910
Puja <pujayadav@gmail.com>
2020-09-29 12:07:42 +0530

    SnakeLadder

---
+++ GameStarted/GameStarted/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GameStarted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            tring snakeladder = null;
+            int start = 0;
+            int max = 100;
+            int current_score = 0;
+            int currentposition = start;
+            Console.WriteLine(" welcome to the snakeladder game:");
+            Console.WriteLine("Do you want to play snake ladder");
+            Console.ReadLine();
+        }
+    }
+}
