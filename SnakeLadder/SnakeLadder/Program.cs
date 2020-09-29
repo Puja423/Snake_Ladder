66771c8b9b822f86b1f806358141d4f762af0e09
Puja <pujayadav@gmail.com>
2020-09-29 13:08:01 +0530

    [Puja] add . Throw a dice

--- SnakeLadder/SnakeLadder/Program.cs
+++ SnakeLadder/SnakeLadder/Program.cs
@@ -6,7 +6,26 @@ namespace SnakeLadder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string snakeladder = null;
+            int start = 0;
+            int max = 100;
+            int current_score = 0;
+            int currentposition = start;
+            Console.WriteLine(" welcome to the snakeladder game:");
+            Console.WriteLine("Do you want to play snake ladder");
+            snakeladder = Console.ReadLine();
+
+            if (snakeladder == "yes")
+            {
+                Random random = new Random();
+                int randomnumber = random.Next(1, 7);
+                Console.WriteLine("throwing a dice.....number is:" + randomnumber);
+            }
+            else
+            {
+                Console.WriteLine("player choose not to play");
+            }
         }
+    }
     }
 }
