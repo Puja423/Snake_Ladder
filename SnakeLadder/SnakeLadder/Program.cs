ce852814001e02cd103c993ae719efe8fe6e1035
Puja <pujayadav@gmail.com>
2020-10-04 21:44:44 +0530

    [puja] add . winning Position

--- SnakeLadder/SnakeLadder/Program.cs
+++ SnakeLadder/SnakeLadder/Program.cs
@@ -1,87 +1,55 @@
 ﻿using System;
 using System.Threading;
 
-namespace SnakeLadder
+namespace Snake_And_Ladders
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string snakeladder = null;
-            int s = 0;
-            int max = 100;
-            int cuurent_score = 0;
-            int currentposition = s;
+            Console.WriteLine("Welcome to the Virtual Snake & Ladders!!");
 
-            Console.WriteLine("Welcome to  snakeladder Game");
-            Console.WriteLine("Do you want to play snakeladder");
-            snakeladder = Console.ReadLine();
-            if (snakeladder.Equals("yes"))
+            var positionOfPlayer1 = 0;
+
+            while (positionOfPlayer1 == 100)
             {
 
+                var NumRoll = RollTheDie();
 
-                while (cuurent_score != 100)
-                {
 
-                
-                        Random random = new Random();
-                        int randomnumber = random.Next(1, 7);
-                        Console.WriteLine("throwing a dice.....:Number is --->" + randomnumber);
-                        Random random1 = new Random();//snake
-                        int randomnumber1 = random1.Next(1, 4);
-                        /**
-                         *If number is 1 --> Player Chooses No Step
-                         *If number is 2--> You got bitten by snake and will move backward with the obtained by dice
-                         *if number is 3---> player choose to play and forward
-                         * 
-                         */
-                        if (randomnumber1 == 1)
-                        {
-                            Console.WriteLine("Player Chooses not to move any step and current position is " + cuurent_score);
+                SnakeOrLadder(ref positionOfPlayer1, NumRoll);
 
-                        }
-                        else if (randomnumber1 == 2)
-                        {
-                            Console.WriteLine("Player got bitten by snake");
-                            if (cuurent_score < 6)
-                            {
-                                cuurent_score = 0;
-                            }
-                            else
-                            {
 
-                                cuurent_score = cuurent_score - randomnumber;
-
-                            }
-                            Console.WriteLine("new Player Position --> " + cuurent_score);
-
-                        }
-                        else
-                        {
-                       
-                        {
-                            int newsum = cuurent_score + randomnumber;
-                            if (newsum <= 100)
-                            {
-                                cuurent_score = newsum;
-                            }
-
-                            Console.WriteLine("new Player Position --> " + cuurent_score);
-                           
-                        }
-
-                    }
-                }
 
             }
 
-
-            else
-            {
-                Console.WriteLine("Player chooses not to play");
-            }
 
         }
+
+        public static int RollTheDie()
+        {
+            return new Random().Next(1, 7);
+        }
+        public static void SnakeOrLadder(ref int positionOfPlayer, int NumRoll)
+        {
+            string[] options = new string[] { "No Play", "Snake", "Ladder" };
+            var option = options[new Random().Next(0, 3)];
+            switch (option)
+            {
+                case "Snake":
+                    positionOfPlayer -= NumRoll;
+                    if (positionOfPlayer < 0)
+                        positionOfPlayer = 0;
+                    break;
+                case "Ladder":
+                    positionOfPlayer += NumRoll;
+                    if (positionOfPlayer > 100)
+                        positionOfPlayer -= NumRoll;
+                    break;
+
+                default:
+                    break;
+            }
+        }
     }
-    
 }
